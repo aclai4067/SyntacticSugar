d673d7943595b691331da6d0c6177ebf6458aafb
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-11 20:49:25 -0500

    Add project files.

---
+++ SyntacticSugar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SyntacticSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var butterflyPredators = new List<string>()
+            {
+                "dogs",
+                "cats",
+                "lizards"
+            };
+
+            var butterflyPrey = new List<string>();
+            butterflyPrey.Add("nectar");
+
+            var butterfly = new bug("Judy", "monarch butterfly", butterflyPredators, butterflyPrey);
+
+            var spiderPredators = new List<string>()
+            {
+                "bigger spiders",
+                "cats",
+                "lizards"
+            };
+
+            var spiderPrey = new List<string>()
+            {
+                "flies",
+                "moths",
+                "butterflies"
+            };
+
+            var spider = new bug("Jonathan", "wolf spider", spiderPredators, spiderPrey);
+
+            Console.WriteLine(spider.Eat("cereal"));
+            Console.WriteLine(spider.Eat("butterflies"));
+            Console.WriteLine($"{spider.FormalName} hunts {spider.PreyList}");
+            Console.WriteLine($"{butterfly.FormalName} hides from {butterfly.PredatorList}");
+        }
+    }
+}
